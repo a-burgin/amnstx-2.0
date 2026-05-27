55cec355dab4b580dde89206a830b5d39b61ddfb
a-burgin <aburgin@amnstx.com>
2026-05-27 04:53:02 -0500

    Delete app directory

--- app/instant-quote/page.js
+++
@@ -1,2 +0,0 @@
-import QuoteCalculator from '../../components/QuoteCalculator';
-export default function Page(){return <section className="simple-page"><div className="eyebrow">Quote</div><h1 className="title">Instant Quote Calculator</h1><QuoteCalculator /></section>}
