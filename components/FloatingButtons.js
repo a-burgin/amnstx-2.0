ea5857b6c241e727fa8b8a97dcb8914951d11dbf
a-burgin <aburgin@amnstx.com>
2026-05-27 10:47:52 -0500

    Add files via upload

---
+++ components/FloatingButtons.js
@@ -0,0 +1,5 @@
+
+const bookingLink = "https://app.bluenotary.us/business/register?referralCode=5ujkkgs0s";
+export default function FloatingButtons() {
+ return <div className="float-buttons"><a className="float-call" href="tel:+12144715358">Call</a><a className="float-text" href="sms:+12144715358">Text</a><a className="float-book" href={bookingLink}>Book</a></div>
+}
