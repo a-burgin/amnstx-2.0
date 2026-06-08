a2eeb35c694ab5aff82fbce893947ef48ecb26b7
a-burgin <aburgin@amnstx.com>
2026-06-07 23:44:31 -0500

    Delete components directory

--- components/StickyActions.js
+++
@@ -1,3 +0,0 @@
-
-const book = 'https://app.bluenotary.us/business/register?referralCode=5ujkkgs0s';
-export default function StickyActions(){return <div className="sticky-actions"><a href="tel:2144715358">Call</a><a href="sms:4692155434">Text</a><a href={book}>Book</a></div>}
